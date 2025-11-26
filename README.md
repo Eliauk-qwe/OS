c483aa20033fd5ea2e322e4be6715ef66f5e88f8
ELiauk-qwe <1552356639@qq.com>
2025-11-26 18:24:05 +0800

    11.26

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # OS
 
-https://pic.leetcode-cn.com/Figures/11_Container_WaterSlide5.PNG
--- chapter-8/kernel/interrupt.h
+++ chapter-8/kernel/interrupt.h
@@ -4,3 +4,4 @@ typedef void* intr_handler;		//将intr_handler定义为void*同类型
 void idt_init(void);
 #endif
 
+    
